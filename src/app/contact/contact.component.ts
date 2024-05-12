3c27672932189dab7ed6077323da2a0a3b7e70c4
HAUCodeGeeks <hau.cg24files@gmail.com>
2024-05-12 23:09:48 +0800

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,14 +13,17 @@ export class ContactComponent {
     subject: '',
     message: ''
   };
+  submissionMessage: string = '';
 
   constructor(private http: HttpClient) {}
 
   onSubmit() {
-    this.http.post<any>('http://localhost:3000/submit', this.formData).subscribe(
+    this.http.post<any>('https://backend-tn8d.onrender.com/contract', this.formData).subscribe(
       response => {
         console.log('Form submitted successfully:', response);
-        // Reset form after submission
+        // Display submission message
+        this.submissionMessage = 'Your message has been sent successfully!';
+        // Reset form after successful submission
         this.formData = {
           fullname: '',
           email: '',
@@ -30,6 +33,8 @@ export class ContactComponent {
       },
       error => {
         console.error('Failed to submit form:', error);
+        // Display error message to the user
+        this.submissionMessage = 'An error occurred while sending your message. Please try again later.';
       }
     );
   }
