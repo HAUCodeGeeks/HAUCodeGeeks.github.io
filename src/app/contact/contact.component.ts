11f97e9ccdd9e63009b12ab86e270db0443335a9
HAUCodeGeeks <hau.cg24files@gmail.com>
2024-05-14 03:14:35 +0800

    updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,26 +15,33 @@ export class ContactComponent {
     message: ''
   };
   readonly APIUrl = "https://backend-tn8d.onrender.com/contact"; // Update with your backend URL
+  isFormComplete: boolean = false;
 
   constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
   onSubmit(event: Event) {
     event.preventDefault(); // Prevent default form submission behavior
 
-    this.http.post<any>(this.APIUrl, this.formData).subscribe(
-      response => {
-        console.log('Form submitted successfully:', response);
-        // Display pop-up notification for successful submission
-        this.showNotification('Your message has been sent successfully!');
-        // Reset form after successful submission
-        this.resetForm();
-      },
-      error => {
-        console.error('Failed to submit form:', error);
-        // Display error message to the user
-        this.showNotification('An error occurred while sending your message. Please try again later.');
-      }
-    );
+    // Check if all required fields are filled out
+    if (this.isFormComplete) {
+      this.http.post<any>(this.APIUrl, this.formData).subscribe(
+        response => {
+          console.log('Form submitted successfully:', response);
+          // Display pop-up notification for successful submission
+          this.showNotification('Your message has been sent successfully!');
+          // Reset form after successful submission
+          this.resetForm();
+        },
+        error => {
+          console.error('Failed to submit form:', error);
+          // Display error message to the user
+          this.showNotification('An error occurred while sending your message. Please try again later.');
+        }
+      );
+    } else {
+      // Display error message if form is not complete
+      this.showNotification('Please fill out all required fields.');
+    }
 
     return false; // Ensure further default behavior is prevented
   }
@@ -53,5 +60,10 @@ export class ContactComponent {
     this.snackBar.open(message, 'Close', {
       duration: 3000, // Duration of the notification in milliseconds
     });
+  }
+
+  checkFormCompletion() {
+    // Check if all required fields are filled out
+    this.isFormComplete = Object.values(this.formData).every(value => !!value);
   }
 }
