b359382308141b810f55aee89e5464fcc886e311
HAUCodeGeeks <hau.cg24files@gmail.com>
2024-05-17 02:31:48 +0800

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -14,63 +13,38 @@ export class ContactComponent {
     subject: '',
     message: ''
   };
-  readonly APIUrl = "https://backend-tn8d.onrender.com/contact"; // Update with your backend URL
-  isFormComplete: boolean = false;
-  isEmailValid: boolean = true;
 
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+  constructor(private http: HttpClient) {}
 
-  onSubmit(event: Event) {
-    event.preventDefault(); // Prevent default form submission behavior
+  onSubmit() {
+    const url = 'https://docs.google.com/forms/u/5/d/e/1FAIpQLSdRjGgfNVyb-xQSGUyy0DcTY1FXWyrHyVhLdzJJqhRDgUpHjA/formResponse';
+    
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 
-    // Check if all required fields are filled out and email is valid
-    if (this.isFormComplete && this.isEmailValid) {
-      this.http.post<any>(this.APIUrl, this.formData).subscribe(
-        response => {
-          console.log('Form submitted successfully:', response);
-          // Display pop-up notification for successful submission
-          this.showNotification('Your message has been sent successfully!');
-          // Reset form after successful submission
-          this.resetForm();
-        },
-        error => {
-          console.error('Failed to submit form:', error);
-          // Display error message to the user
-          this.showNotification('An error occurred while sending your message. Please try again later.');
-        }
-      );
-    } else {
-      // Display error message if form is not complete or email is invalid
-      let errorMessage = this.isEmailValid ? 'Please fill out all required fields.' : 'Please provide a valid email address.';
-      this.showNotification(errorMessage);
-    }
+    const body = new URLSearchParams();
+    body.append('entry.2005620554', this.formData.fullname);
+    body.append('entry.1045781291', this.formData.email);
+    body.append('entry.300625256', this.formData.subject);
+    body.append('entry.1219774711', this.formData.message);
 
-    return false; // Ensure further default behavior is prevented
+    this.http.post(url, body.toString(), { headers }).subscribe(
+      () => {
+        console.log('Your message has been sent successfully!');
+        this.resetForm();
+      },
+      (error) => {
+        console.error('Error submitting form:', error);
+        console.log('Error submitting form. Please try again later.');
+      }
+    );
   }
 
   resetForm() {
-    // Reset form fields
     this.formData = {
       fullname: '',
       email: '',
       subject: '',
       message: ''
     };
-  }
-
-  showNotification(message: string) {
-    this.snackBar.open(message, 'Close', {
-      duration: 3000, // Duration of the notification in milliseconds
-    });
-  }
-
-  checkFormCompletion() {
-    // Check if all required fields are filled out
-    this.isFormComplete = Object.values(this.formData).every(value => !!value);
-  }
-
-  checkEmailValidity() {
-    // Check if email contains "@" symbol
-    this.isEmailValid = this.formData.email.includes('@');
   }
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -17,36 +17,36 @@ export class GalleryComponent implements OnInit {
   @Input() indicators = true;
   @Input() controls = true;
   @Input() autoSlide = true;
-  @Input() slideInterval = 3000;
+  @Input() slideInterval = 5000;
 
   selectedIndex = 0;
   titleIndex = 0;
   descIndex = 0;
   intervalId: any;
 
-  t1: string = `hello`;
-  d1: string = `Students under the School of Computing department are eligible to join without any specific requirements.`;
+  t1: string = `Web Design Competition`;
+  d1: string = `Students who participated in our Web Competition have displayed their utmost skills and talent in Web design. We\’re truly grateful for all the participants of this special event. As we recognize various talented students who shared their passion with our course Web Development.`;
 
-  t2: string = `hi`;
-  d2: string = `minion`;
+  t2: string = `Days with The Foxes`;
+  d2: string = `On March 12 and 13, Holy Angel University\’s School of Computing hosted the event “Days With The Foxes” which was guided by the SOC\’s Student Council and Organizations. This very event welcomed students from various schools as they were introduced to and walked through the life of a fox under different courses and majors of the School of Computing.`;
 
-  t3: string = `boo`;
-  d3: string = `wahhh`;
+  t3: string = `General Assembly`;
+  d3: string = `Code Geeks took part in this annual event to welcome the School of Computing students for the Academic Year 2023-2024, providing an opportunity to connect with the administration, faculty, staff, the college student council, and various organizations. The event concluded with joy and success as it marks the start of the Academic year for the Foxes to Make IT happen!`;
 
-  t4: string = `pengu`;
-  d4: string = `noot noot`;
+  t4: string = `Visit to IMMFI`;
+  d4: string = `On the visit to Inocencio Magtoto Memorial Foundation, Inc. (IMMFI) last April 23, We delivered practical training on website management, covering fundamental WordPress concepts and important upkeep duties to assist the team in improving their online presence. It was a rewarding experience. We are incredibly appreciative of the Foundation's staff for their kind hospitality and engaged involvement, which made our stay genuinely remarkable.`;
 
-  t5: string = `hello`;
-  d5: string = `Students under the School of Computing department are eligible to join without any specific requirements.`;
+  t5: string = `Org Festival 2023`;
+  d5: string = `A new chapter begins! ✨ The very first event that Code Geeks participated in was the Org Festival held on July 14, 2023, from students across the Holy Angel University campus. The org festival was a fantastic experience for everyone which was made especially for the freshmen students who were new to the grounds of Holy Angel University.`;
 
-  t6: string = `hi`;
-  d6: string = `minion`;
+  t6: string = `Outreach Initiative`;
+  d6: string = `As we look back on the incredible day of March 15, 2024, when SOC-CSC, Code Geeks, and other SOC groups came together for an outreach program at Sapang Libutad's Rafael Lazatin Integrated School. Every organization covers a separate grade level while enlightening students on a range of technology-related subjects.`;
 
-  t7: string = `boo`;
-  d7: string = `wahhh`;
+  t7: string = `New Normal Workplace`;
+  d7: string = `We extend our heartfelt gratitude to the students who attended our first webinar for this A.Y., with the title "Remote: A New Normal Workplace," on November 6, 2023. Special thanks to Mr. John Rey Casingal for sharing his invaluable insights all about working remotely as a new setup considered as the new normal in the workplace.`;
 
-  t8: string = `pengu`;
-  d8: string = `noot noot`;
+  t8: string = `SEO Rank & Rise Webinar`;
+  d8: string = `With the title "SEO Rank & Rise: The Beginner's Blueprint to Web Winning," a webinar held on February 17, 2024, hosted by the Code Geeks, was a significant experience for web development students. Special thanks to Mr. Adrian T. Dioneso for his invaluable insights as he introduced us to the importance of Search Engine Optimization as we associate it with our very course, Web Development.`;
   
   title: string[] = [
     this.t1,
