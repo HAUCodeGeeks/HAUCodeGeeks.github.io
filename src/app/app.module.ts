9daf0dd01f64838fb7d2415fa88182e373e8ad75
HAUCodeGeeks <hau.cg24files@gmail.com>
2024-05-18 01:51:52 +0800

    FINAL UPDATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
